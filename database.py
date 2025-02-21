9a66632c4703cd01eb1773307bf56ad4b65b9b0e
Jackie <kennedy.lsd32@gmail.com>
2025-02-21 22:28:54 +0100

    update db connection

--- database.py
+++ database.py
@@ -2,12 +2,20 @@ from typing import Type
 
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import text
+from sqlalchemy.pool import AsyncAdaptedQueuePool
+
+
 
 from models import Base
 
-DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5432/ww"
+DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5432/w"
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(DATABASE_URL, echo=False, poolclass=AsyncAdaptedQueuePool,
+    pool_pre_ping=True,
+    pool_size=5,
+    max_overflow=10,
+    pool_timeout=30)
 
 AsyncSessionLocal = sessionmaker(
     class_=AsyncSession,
@@ -15,6 +23,18 @@ AsyncSessionLocal = sessionmaker(
     autoflush=False,
     autocommit=False
 )
+
+async def check_db_connection():
+    if engine.pool is None:
+        raise RuntimeError("Database engine is not initialized.")
+    
+    try:
+        async with engine.begin() as conn:
+            await conn.execute(text("SELECT 1") ) 
+        print("Database connection successful.")
+    except Exception as e:
+        raise RuntimeError("Failed to connect to the database.") from e
+    
 
 async def get_db():
     async with AsyncSessionLocal(bind=engine) as session:
@@ -28,4 +48,5 @@ async def create_tables():
 
 
 async def close():
-    await engine.dispose()
+    if engine is not None:
+        await engine.dispose(close=True)
--- models.py
+++ models.py
@@ -1,4 +1,6 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from typing import List
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
 class Base(DeclarativeBase):
@@ -11,5 +13,16 @@ class Car(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     model: Mapped[str]
     price: Mapped[int]
+    owner_id: Mapped[int] = mapped_column(ForeignKey("owners.id"))
+    owner: Mapped["Owner"] = relationship("Owner", back_populates="cars")
+
+
+class Owner(Base):
+    __tablename__ = "owners"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True)
+    age: Mapped[int] = mapped_column()
+    cars: Mapped[List["Car"]] = relationship("Car", back_populates="owner")
 
     
